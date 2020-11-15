8ad345298ecfdacac3ab5e5a957e14a425906100
ivan1910 <ivanraykovskiy@gmail.com>
2020-11-15 22:50:21 +0200

    WPF Project

---
+++ Tests/KPZProjectConsole/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace KPZProjectConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
